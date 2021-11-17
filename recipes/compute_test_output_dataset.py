b9125b377c0c4f7fb34b431b998ec92b64fda1b3
admin <admin>
2021-11-17 09:42:26 +0000

    Recipe DEMO_LOAN_DEFAULT.compute_test_output_dataset saved through notebook notebook editor for compute_test_output_dataset

--- recipes/compute_test_output_dataset.py
+++ recipes/compute_test_output_dataset.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -7,14 +8,21 @@ from dataiku import pandasutils as pdu
 scored_data = dataiku.Dataset("scored_data")
 scored_data_df = scored_data.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+scored_data_df.head()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
 test_output_dataset_df = scored_data_df # For this sample code, simply copy input to output
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+test_output_dataset_df["new_column"]=test_output_dataset_df["Amount_Requested"]/10
+test_output_dataset_df.head()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 test_output_dataset = dataiku.Dataset("test_output_dataset")
 test_output_dataset.write_with_schema(test_output_dataset_df)
